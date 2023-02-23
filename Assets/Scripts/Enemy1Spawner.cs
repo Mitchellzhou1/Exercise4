00e3642c856b8e130d53438acaefb0b1729cb947
Yanglin Tao <yt2061@nyu.edu>
2023-02-23 01:14:21 -0500

    Fixed minor bugs in Level 1

--- Assets/Scripts/Enemy1Spawner.cs
+++ Assets/Scripts/Enemy1Spawner.cs
@@ -9,7 +9,7 @@ public class Enemy1Spawner : MonoBehaviour
     IEnumerator Start()
     {
         for (int i = 0; i < 2000; i++) {
-            Vector2 spawnPos = new Vector2(Random.Range(-8.5f, 8.5f), Random.Range(-10, 10));
+            Vector2 spawnPos = new Vector2(Random.Range(8.5f, 17f), Random.Range(-10, 10));
             Instantiate(gorillaPrefab, spawnPos, Quaternion.identity);
             yield return new WaitForSeconds(.4f);
         }
