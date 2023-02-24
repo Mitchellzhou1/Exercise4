7b06bbc707d0a187df3904c858150a8e93283c1d
xxiaoling <xz3490@nyu.edu>
2023-02-23 21:45:58 -0500

    fixed game transition and added sound effect to level 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     public TMPro.TextMeshProUGUI scoreUI;
     public TMPro.TextMeshProUGUI lifeUI;
 
-    private bool defeatedBoss = false;
+    // private bool defeatedBoss = false;
 
     private void Awake()
     {   
@@ -73,9 +73,9 @@ public class GameManager : MonoBehaviour
         swapToEnd(3);
     }
 
-    public void beatGame(){
-        defeatedBoss = true;
-    }
+    // public void beatGame(){
+    //     defeatedBoss = true;
+    // }
 
 //
 
@@ -110,14 +110,14 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene("Level 3");
         }
         }
-    if (levelName == "Level 3")
-        {
-        if (defeatedBoss)
-        {
-            Destroy(gameObject); 
-            SceneManager.LoadScene("Victory");
-        }
-        }
+    // if (levelName == "Level 3")
+    //     {
+    //     if (defeatedBoss)
+    //     {
+    //         Destroy(gameObject); 
+    //         SceneManager.LoadScene("Victory");
+    //     }
+    //     }
     }
     IEnumerator swapToEnd (int seconds) {
         int counter = seconds;
